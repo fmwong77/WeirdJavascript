3a1c2d832111a1c3d3a8470933ebdeb38054dc54
fmwong77 <fongmeiw@gmail.com>
2020-05-13 10:36:58 -0500

    built in function constructors

--- BuiltInFunctionConstructors.js
+++ BuiltInFunctionConstructors.js
@@ -7,3 +7,9 @@ console.log('John'.isLengthGreaterThan(3));
 Number.prototype.isPositive = function () {
 	return this > 0;
 };
+
+Array.prototype.myCustomerFeature = 'cool';
+var arr = [1, 2, 3];
+for (const ele of arr) {
+	console.log(ele);
+}
