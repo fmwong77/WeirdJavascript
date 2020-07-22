97fdcc3a078e5c22ea86535e4b29de94d435d815
fmwong77 <fongmeiw@gmail.com>
2020-07-22 16:46:04 -0500

    array functions - filter, includes

---
+++ ArrayFunctions.js
@@ -0,0 +1,8 @@
+const myArray = [1, 3, 5, 7, 9];
+
+// filter returns new array
+console.log(myArray.filter((ele) => ele > 4));
+console.log(myArray);
+
+// includes([value],[position])
+console.log(myArray.includes(3, 0));
