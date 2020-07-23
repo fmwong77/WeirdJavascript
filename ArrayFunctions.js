96cbfc2d86f6c834f0461b5838a0ca37e04676d8
fmwong77 <fongmeiw@gmail.com>
2020-07-22 20:26:04 -0500

    array.find()

--- ArrayFunctions.js
+++ ArrayFunctions.js
@@ -6,3 +6,18 @@ console.log(myArray);
 
 // includes([value],[position])
 console.log(myArray.includes(3, 0));
+
+const o1 = { id: 1 };
+const o2 = { id: 2 };
+const o3 = { id: 3 };
+
+const newArray = [o1, o2, o3];
+console.log(newArray.includes(o1)); // true
+
+// find
+console.log(
+	'find',
+	myArray.find((ele) => ele === 5)
+);
+
+console.log(newArray.find((arr) => arr.id === 1));
