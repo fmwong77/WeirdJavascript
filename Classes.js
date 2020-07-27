297e2e4f9cd4be699a023dca746616f0e3a7e3e4
fmwong77 <fongmeiw@gmail.com>
2020-07-27 16:17:23 -0500

    define function in class

--- Classes.js
+++ Classes.js
@@ -1,17 +1,39 @@
-//classes are not hoisted, it must be defined before using it
-try {
-	let p = new Polygon(1, 2);
-	console.log('Polygon p:', p);
-} catch (exception) {
-	console.log(exception.name + ': ' + exception.message);
-}
+// //classes are not hoisted, it must be defined before using it
+// try {
+// 	let p = new Polygon(1, 2);
+// 	console.log('Polygon p:', p);
+// } catch (exception) {
+// 	console.log(exception.name + ': ' + exception.message);
+// }
+
+// class Polygon {
+// 	constructor(height, width) {
+// 		this.height = height;
+// 		this.width = width;
+// 	}
+// }
+
+// p = new Polygon(1, 2);
+// console.log('Polygon p:', p);
 
 class Polygon {
-	constructor(height, width) {
-		this.height = height;
-		this.width = width;
+	constructor(a) {
+		this.sideLengths = a;
+	}
+
+	perimeter() {
+		let result = this.sideLengths[0];
+		for (let i = 1; i < this.sideLengths.length; i++) {
+			result += this.sideLengths[i];
+		}
+		return result;
 	}
 }
 
-p = new Polygon(1, 2);
-console.log('Polygon p:', p);
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+// console.log(square.perimeter());
+// console.log(pentagon.perimeter());
