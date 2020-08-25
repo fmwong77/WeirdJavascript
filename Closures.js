f44de568c2710332aa2402546a33cb09cb3375d8
fmwong77 <fongmeiw@gmail.com>
2020-08-25 18:42:56 -0500

    getter and setter in javascript

--- Closures.js
+++ Closures.js
@@ -1,8 +1,28 @@
 function greet(whattosay) {
-	return function (name) {
-		console.log(whattosay + ' ' + name);
-	};
+  return function (name) {
+    console.log(whattosay + " " + name);
+  };
 }
 
-var sayHi = greet('Hi');
-sayHi('Tony');
+var sayHi = greet("Hi");
+sayHi("Tony");
+
+var v1 = 1;
+function f1() {
+  f2();
+}
+
+function f2() {
+  var v1 = 2;
+  console.log(v1);
+}
+
+f1();
+
+class x {
+  get y() {
+    return 42;
+  }
+}
+
+x.get("y")();
---
+++ FunctionGetter.js
@@ -0,0 +1,20 @@
+const obj = {
+  log: ["a", "b", "c"],
+  latest: function () {
+    if (this.log.length === 0) {
+      return undefined;
+    }
+    return this.log[this.log.length - 1];
+  },
+  get getterFunction() {
+    if (this.log.length === 0) {
+      return undefined;
+    }
+    return this.log[this.log.length - 1];
+  },
+};
+
+console.log(obj.latest());
+console.log(obj.getterFunction);
+
+// expected output: "c"
